25b153d942136b7b7cc2a9f4f3bd9764faeec606
Markus Skov <69841783+markusskov@users.noreply.github.com>
2022-02-14 22:13:57 +0100

    final test

--- components/intro/introImage.js
+++ components/intro/introImage.js
@@ -8,7 +8,7 @@ const ImageWrapper = styled.div`
   justify-content: flex-end;
 `;
 
-const Image = styled.img`
+const HeaderImg = styled.img`
   object-fit: contain;
   height: 90%;
   width: 90%;
@@ -17,7 +17,7 @@ const Image = styled.img`
 export default function IntroImage() {
   return (
     <ImageWrapper>
-      <Image src="../images/introImage.png" alt="Intro-Image" />
+      <HeaderImg src="/images/introimage.svg" alt="Intro-Image" />
     </ImageWrapper>
   );
 }
